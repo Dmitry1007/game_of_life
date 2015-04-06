86573887721cc0096fe061d3279c3a5df1098736
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-06 17:06:09 -0600

    conways grid oscillation

---
+++ conways_oscillators.rb
@@ -0,0 +1,27 @@
+class Grid
+  attr_accessor :grid
+  def initialize
+    @grid = ["[0,0,0]\n[0,0,0]\n[0,0,0]"]
+  end
+
+  def column
+    @grid = ["[0,X,0]\n[0,X,0]\n[0,X,0]"]
+  end
+
+  def oscillate
+    if @grid == ["[0,X,0]\n[0,X,0]\n[0,X,0]"]
+      @grid = ["[0,0,0]\n[X,X,X]\n[0,0,0]"]
+    elsif @grid == ["[0,0,0]\n[X,X,X]\n[0,0,0]"]
+      @grid = ["[0,X,0]\n[0,X,0]\n[0,X,0]"]
+    end
+  end
+end
+
+gridastic = Grid.new
+puts gridastic.grid
+gridastic.column
+4.times do
+  puts "\n"
+  puts gridastic.oscillate
+  puts " "
+end
